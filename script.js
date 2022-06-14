de4274a2f1f736779933dfb0815ad514ceda4e0c
ddao19 <dung.dao19@outlook.com>
2022-06-14 18:15:52 -0500

    added delete and tab button feature. Made some css updates

--- script.js
+++ script.js
@@ -1,35 +1,73 @@
 let myLeads = [];
 const error = document.querySelector(".error");
 const inputEl = document.getElementById("input-el");
-const buttonEl = document.getElementById("input-btn");
+const inputBtnEl = document.getElementById("input-btn");
+const deleteBtnEl = document.getElementById("delete-btn");
+const tabBtnEl = document.getElementById("tab-btn");
 const ulEl = document.getElementById("ul-el");
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
-buttonEl.addEventListener("click", function (event) {
-  // Error handling and styling or error
-  if (inputEl.value === "") {
-    error.setAttribute("id", "errorMsg");
-    error.textContent = "Please enter a valid URL";
-  } else if (inputEl.value != "") {
-    error.removeAttribute("id");
-    error.textContent = "";
-
-    // let location = window.location.href
-    let leads = inputEl.value;
-    myLeads.push(leads);
-    inputEl.value = ""; // clear input text field
-    sortLeads();
-  }
-});
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  render(myLeads);
+}
 
 // this fucntion helps with displaying the leads in order once entered
-function sortLeads() {
+function render(leads) {
   let listItems = "";
-  for (let i = 0; i < myLeads.length; i++) {
+
+  for (let i = 0; i < leads.length; i++) {
     listItems += `
     <li class='list-item'>
-      <a class="underline" href="${myLeads[i]}" target="_blank">${myLeads[i]}</a>
+      <a class="underline" href="${leads[i]}" target="_blank">${leads[i]}</a>
     </li>`;
   }
 
   ulEl.innerHTML = listItems;
 }
+
+// Handle input button click
+inputBtnEl.addEventListener("click", function (event) {
+  // Error handling and styling or error
+  if (inputEl.value === "" || inputEl.value.replace(/\s+/g, "").length == 0) {
+    inputEl.focus();
+    inputEl.value = "";
+
+    error.setAttribute("id", "errorMsg");
+    error.textContent = "Please enter a valid URL";
+  } else if (inputEl.value !== "") {
+    error.removeAttribute("id");
+    error.textContent = "";
+
+    let leads = inputEl.value;
+    myLeads.push(leads);
+    inputEl.value = ""; // clear input text field
+
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+
+    render(myLeads);
+  }
+});
+
+tabBtnEl.addEventListener("click", function () {
+  chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+  });
+});
+
+// Handle delete button click
+deleteBtnEl.addEventListener("click", function () {
+  if (myLeads.length !== 0) {
+    error.removeAttribute("id");
+    error.textContent = "";
+
+    localStorage.clear();
+    myLeads = [];
+    render(myLeads);
+  } else if (myLeads.length === 0) {
+    error.setAttribute("id", "errorMsg");
+    error.textContent = "There are no leads to delete.";
+  }
+});
